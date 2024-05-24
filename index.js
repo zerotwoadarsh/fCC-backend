7a4b339b9bc522e2bb7991fa032b7b62941f24f6
zerotwoadarsh <zerotwoadarsh@gmail.com>
2024-05-24 14:30:16 +0530

    new start

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const express = require ('express')
+require('dotenv').config()
 
+const express = require('express');
 const app = express()
-
-const port = 3000
+const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World !')
+    res.send('Hey')
 })
 
 app.get('/twitter', (req, res) => {
-    res.send('Hello Twitter !')
+    res.send('here is twitter')
 })
 
-app.listen(port, () =>{
-    console.log("Server is listening ");
+app.listen(process.env.PORT, () => {
+    console.log(port)
 })
